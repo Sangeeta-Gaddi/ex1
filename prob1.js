ad2d618c67a863cf597a699b2889fb95b9fa3eb1
Sangeeta-Gaddi <137263910+Sangeeta-Gaddi@users.noreply.github.com>
2024-03-07 00:28:33 -0800

    Add files via upload

---
+++ prob1.js
@@ -0,0 +1,31 @@
+//Declare different types of variable and print type of each//
+
+//We can declare variable using let, var  and const keyword//
+
+//using let keyword//
+var num=45;
+console.log(typeof(num));
+
+let str="Nishita";
+console.log(typeof(str));
+
+const bool=false;
+console.log(typeof(bool));
+
+ var array=[1,2,3,4];
+ console.log(typeof array);
+
+ let obj={name:"Nishita", dob:17/5/2001};
+ console.log(typeof(obj));
+
+ const func=function(){};
+ console.log(typeof(func));
+
+ //output//
+//  number
+// string
+// boolean
+// object
+// object
+// function
+
